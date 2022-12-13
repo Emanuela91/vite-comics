ce96e11a792184f3e71f99c67bdb36d8cfd2d398
Emanuela91 <emanuela.marson91@gmail.com>
2022-12-13 16:26:14 +0100

    aggiunto nel componente Header i links, logo e v-for per links

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script>
+import HeaderVue from './components/Header.vue';
 
+export default {
+  name: "App",
+  components: {
+    HeaderVue,
+  }
+}
 </script>
 
 <template>
-
+  <HeaderVue />
+  <main>
+    <h1>Content goes here!</h1>
+  </main>
 </template>
 
-<style>
-
+<style lang="scss">
+@use './styles/general.scss' as*;
+@use './styles/partials/variables' as*;
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false,
+                }
+            ]
+        }
+    }
+}
+
+</script>
+
+<template>
+    <div class="logo">
+        <img src="../assets/img/dc-logo.png" alt="logoDC">
+    </div>
+
+    <nav>
+        <ul>
+            <li v-for="(link, index) in links" key="index">
+                <a :class="{ active: link.current }" :href="link.url">
+                    {{ link.text }}
+                </a>
+            </li>
+        </ul>
+    </nav>
+
+</template>
+
+<style lang="scss">
+@use '../styles/general.scss' as*;
+@use '../styles/partials/variables' as*;
+</style>
